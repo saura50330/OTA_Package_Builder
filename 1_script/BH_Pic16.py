04def9a1d9f1b398a361cee7a062d0169b1dcf43
saura50330 <nan1ban@yahoo.com>
2019-06-13 03:36:16 +0530

    updated script for credential file and fectory file

--- 1_script/BH_Pic16.py
+++ 1_script/BH_Pic16.py
@@ -1,3 +1,4 @@
+#C:\Python27\python.exe
 import sys
 import time
 import serial
@@ -15,7 +16,7 @@ import os
 FLASH_SECTOR_SIZE = 0x20  # minimum size which can be eraced 32 bytes
 
 APP_START_ADD = 0x2A0
-APP_END_ADD   = 0x800
+APP_END_ADD	  = 0x800
 
 #EEPROM ADDRESSES
 EEP_START_ADD = 0xF000
@@ -23,7 +24,7 @@ DEF_EEP_DATA = 0xFF
 #--------Fectory 0xF0 is added by default ----------
 EEP_FECT_DATA_VALID = 255
 EEP_FECT_BL_VER = 254
-EEP_FECT_DEV_TYP = 251   
+EEP_FECT_DEV_TYP = 252	 
 EEP_FECT_LID_3 = 251
 EEP_FECT_LID_2 = 250
 EEP_FECT_LID_1 = 249
@@ -41,43 +42,43 @@ EEP_FDR_APP_VALID = 240
 
 #-----------Command-------
 FLSH_WR_CMD = 0x01
-EEP_WR_CMD  = 0x02
+EEP_WR_CMD	= 0x02
 FLSH_RD_CMD = 0x03
-EEP_RD_CMD  = 0x04
-PING_CMD    = 0x05
-RESET_CMD   = 0x06
-FLASH_CMD   = 0x07
+EEP_RD_CMD	= 0x04
+PING_CMD	= 0x05
+RESET_CMD	= 0x06
+FLASH_CMD	= 0x07
 DUMMY_DEF_CMD = 0xFF
 #--------
 RESPONCE_BYTE_LEN = 5
 BREAK = 0.1 # time gap between two TX command
 
 #-------DEvice type values-------
-NO_DEVICE 0xFF
-DEMO 0x01
-WALL_SWITCH 0x02
+NO_DEVICE = 0xFF
+DEMO = 0x01
+WALL_SWITCH = 0x02
 
 Hex_File_Path=""
 # "C:\Python27\python.exe C:\Users\acer\Desktop\MASTER_DOCUMENT_V1\START_UP\microchip\boot_loder\bt_ldr\2_Bootloader_Design\boot_loader_Script\BH_Pic16.py"
 resp_info=[]
-ext_add =0
+ext_add = 0
 image_checksum = 0
 byt_cnt = 0
 def split_array(arr, size):
-     arrs = []
-     while len(arr) > size:
-         pice = arr[:size]
-         arrs.append(pice)
-         arr   = arr[size:]
-     arrs.append(arr)
-     return arrs
+	 arrs = []
+	 while len(arr) > size:
+		 pice = arr[:size]
+		 arrs.append(pice)
+		 arr   = arr[size:]
+	 arrs.append(arr)
+	 return arrs
 def split_by_n( seq, n ):
-    """A generator to divide a sequence into chunks of n units."""
-    while seq:
-        yield seq[:n]
-        seq = seq[n:]
+	"""A generator to divide a sequence into chunks of n units."""
+	while seq:
+		yield seq[:n]
+		seq = seq[n:]
 def Flash_verify():
-    address = APP_START_ADD
+	address = APP_START_ADD
 	list_read=[FLSH_RD_CMD,(address&0xFF),((address>>8)&0xFF)]
 	
 	while(address < APP_END_ADD):
@@ -96,25 +97,26 @@ def Flash_verify():
 		ser.reset_output_buffer()
 		
 def Write_Fect_OTA_file(file_path,Device_Type_temp):
-    thefile = open(file_path, 'w')
-	thefile.write(str(FLASH_CMD) + "\n") # insert reflash command
-	thefile.write(str(DUMMY_DEF_CMD) + " 0 0 0\n") # insert dummy command for delay to recover
-	thefile.write(str(DUMMY_DEF_CMD) + " 0 0 0\n") # insert dummy command for delay to recover
-	thefile.write(str(DUMMY_DEF_CMD) + " 0 0 0\n") # insert dummy command for delay to recover
-	thefile.write(str(DUMMY_DEF_CMD) + " 0 0 0\n") # insert dummy command for delay to recover
-    
-    
-    thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FECT_DEV_TYP) +" 240 " + str(Device_Type_temp) + "\n") # write device type
-    
-    thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FECT_LID_0) +" 240 1\n") # write DEVICE ID  TODO: this id t be maintaind to provide uniqu id every time it is called
-    thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FECT_LID_1) +" 240 1\n") # write DEVICE ID
-    thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FECT_LID_2) +" 240 1\n") # write DEVICE ID
-    thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FECT_LID_3) +" 240 1\n") # write DEVICE ID
+	
+	#FDR
+	#SEND DEF MAC
+	New_Hex_File_Path=os.path.splitext(file_path)[0]
+	New_Hex_File_Path=New_Hex_File_Path + "DID_01010101.FECT" #TODO:  update this with DID serial no
+	thefile = open(New_Hex_File_Path, 'w')
+	
+	thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FECT_DEV_TYP) +" 240 " + str(Device_Type_temp) + "\n") # write device type
+	
+	thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FECT_LID_0) +" 240 1\n") # write DEVICE ID	 TODO: this id t be maintaind to provide uniqu id every time it is called
+	thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FECT_LID_1) +" 240 1\n") # write DEVICE ID
+	thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FECT_LID_2) +" 240 1\n") # write DEVICE ID
+	thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FECT_LID_3) +" 240 1\n") # write DEVICE ID
 
-    thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FECT_DATA_VALID) +" 240 1\n") # Fectory update completed
-    # verify data
-    
-    thefile.close()	
+	thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FECT_DATA_VALID) +" 240 1\n") # Fectory update completed
+	
+	
+	# verify data
+	
+	thefile.close()	
 def Write_Hex_to_String_64(file_path,Device_Type_temp):
 	global temp
 	bl_frame_64_data = ""
@@ -130,9 +132,9 @@ def Write_Hex_to_String_64(file_path,Device_Type_temp):
 	try:
 		f = open(file_path,'r')
 		for line in f:
-			length = int(line[1:3],16)/2 			# data length in bytes# 1st two chrecter are length  # 1:3 mease extrect 1 and 2nd charecter from line #int(line[1:2], 16)
-			address = int(line[3:7],16) | (ext_add<<16) 	# hex file address	# 3rd 4th 5th and 6th char are address 
-			dat_type =  int(line[7:9],16)
+			length = int(line[1:3],16)/2			# data length in bytes# 1st two chrecter are length	 # 1:3 mease extrect 1 and 2nd charecter from line #int(line[1:2], 16)
+			address = int(line[3:7],16) | (ext_add<<16)		# hex file address	# 3rd 4th 5th and 6th char are address 
+			dat_type =	int(line[7:9],16)
 			line_lnt=len(line)
 			if (line_lnt > 3):
 				data=line[9:(line_lnt-3)]  # data field
@@ -180,9 +182,8 @@ def Write_Hex_to_String_64(file_path,Device_Type_temp):
 	thefile.write(str(DUMMY_DEF_CMD) + " 0 0 0\n") # insert dummy command for delay to recover
 	thefile.write(str(DUMMY_DEF_CMD) + " 0 0 0\n") # insert dummy command for delay to recover
 	thefile.write(str(DUMMY_DEF_CMD) + " 0 0 0\n") # insert dummy command for delay to recover
-    
-	thefile.write(str(EEP_WR_CMD) + " "+ EEP_FDR_APP_VALID +" 240 "+str(DEF_EEP_DATA)+\n") # make application Invalid basically erases device type stored in FDR 
-	thefile.write(str(EEP_WR_CMD) + " "+ EEP_FDR_APP_VERSN +" 240 "+str(DEF_EEP_DATA)+\n") # make application version 255 
+	thefile.write( str(EEP_WR_CMD) + " "+ str(EEP_FDR_APP_VALID) +" 240 " + str(DEF_EEP_DATA) + "\n") # make application Invalid basically erases device type stored in FDR 
+	thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FDR_APP_VERSN) +" 240 "+str(DEF_EEP_DATA) + "\n") # make application version 255 
 	for index in range(len(list_b)):
 		list_Temp = list_b[index]
 		list_Temp = list(split_by_n(list_Temp,2))
@@ -205,7 +206,7 @@ def Write_Hex_to_String_64(file_path,Device_Type_temp):
 		del bl_data_64_farme[:]
 	eep_frame=list(split_array(eep_frame,4)) # 16 cherecters in eep write command
 	if(len(eep_frame)>1):
-		print("writing  EEPROM data")
+		print("writing	EEPROM data")
 		#print len(eep_frame)
 		for index in range(len(eep_frame)):
 			#print (eep_frame[index]),
@@ -215,69 +216,67 @@ def Write_Hex_to_String_64(file_path,Device_Type_temp):
 			temp_data=temp_data.replace("]", "")
 			thefile.write(temp_data + "\n")
 	image_checksum = image_checksum & 0xFF
-	print "Image checksum : " + str(image_checksum) + "data lnt: " + str(byt_cnt)
-	thefile.write(str(EEP_WR_CMD) + " "+ EEP_FDR_APP_CHKSM + " 240 " + str(image_checksum) + "\n") # write image_checksum
-	thefile.write(str(EEP_WR_CMD) + " "+ EEP_FDR_APP_VALID + " 240 " + str(Device_Type_temp) + "\n") # make application valid
+	print "Image checksum : " + str(image_checksum) + ", data lnt: " + str(byt_cnt)
+	thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FDR_APP_CHKSM) + " 240 " + str(image_checksum) + "\n") # write image_checksum
+	thefile.write(str(EEP_WR_CMD) + " "+ str(EEP_FDR_APP_VALID) + " 240 " + str(Device_Type_temp) + "\n") # make application valid
 	thefile.close()	
 	
 def Create_Credential(file_path):	
-    thislist = ["DEV_UN","DEV_PW","DEV_TYP","DEV_ID","DEV_RES1","DEV_RES2","MAST_MAC", "USER_MAC","USER_INFO","TEMP_MAC_RAND","TIME_VALID","CRED_TYPE"] #do not 
-    n = 0
-    for i in thislist:
-        temp_data = raw_input("Enter " + i + ": ")
-        thislist[n] = temp_data.replace(',','') # no comma alloud
-        n = n+1
-    str1 = ','.join(thislist) #comma is used to seprate cred
-    print str1
-    
-    New_Hex_File_Path=os.path.splitext(file_path)[0]
+	thislist = ["DEV_UN","DEV_PW","DEV_TYP","DEV_ID","DEV_RES1","DEV_RES2","MAST_MAC", "USER_MAC","USER_INFO","TEMP_MAC_RAND","TIME_VALID","CRED_TYPE"] #do not 
+	n = 0
+	for i in thislist:
+		temp_data = raw_input("Enter " + i + ": ")
+		thislist[n] = temp_data.replace(',','') # no comma alloud
+		n = n+1
+	str1 = ','.join(thislist) #comma is used to seprate cred
+	print str1
+	
+	New_Hex_File_Path=os.path.splitext(file_path)[0]
 	New_Hex_File_Path=New_Hex_File_Path + "_Usr"+ thislist[7] +".CRED" # 7 is user cred MAC
 	thefile = open(New_Hex_File_Path, 'w')
 	thefile.write(str1) # insert reflash command
-    thefile.close()
-    
-def send_cred(cred,User_MAC):
-    #convert and send big frame
-    #thislist = ["saura","1234","1","12,34,5F,DF","DEV_RES1","DEV_RES2","44,55,77,8F", "2F,1F,2F,80","Employee","1F","10/10/2019,10/10/2020","NORMAL"]
-    #mac_temp = "2F,1F,2F,80"
+	thefile.close()
+	
 def send_cred(cred_list,User_MAC_data):
-    #convert and send big frame
-    
-    cred_send =[0x51] # CRED command 
-    cred_send.append(0x00)#rendom ENC keys
-    cred_send.append(0x00)#rendom ENC keys
-    cred_send.append(int(cred_list[9],16)) #RAND ID (key ?)
-    
-    temp_data = User_MAC_data.split(",")
-    # perform conversion 
+	#thislist = ["saura","1234","1","12,34,5F,DF","DEV_RES1","DEV_RES2","44,55,77,8F", "2F,1F,2F,80","Employee","1F","10/10/2019,10/10/2020","NORMAL"]
+	#mac_temp = "2F,1F,2F,80"
+	#convert and send big frame
+	
+	cred_send =[0x51] # CRED command 
+	cred_send.append(0x00)#rendom ENC keys
+	cred_send.append(0x00)#rendom ENC keys
+	cred_send.append(int(cred_list[9],16)) #RAND ID (key ?)
+	
+	temp_data = User_MAC_data.split(",")
+	# perform conversion 
   
-    for i in range(0, len(temp_data)): 
-        temp_data[i] = (int(temp_data[i],16))
-    
+	for i in range(0, len(temp_data)): 
+		temp_data[i] = (int(temp_data[i],16))
+	
   
-    cred_send.append(' '.join(str(e) for e in temp_data))             #user mac
+	cred_send.append(' '.join(str(e) for e in temp_data))			  #user mac
 
-    temp_data = cred_list[3].split(",")
-    for i in range(0, len(temp_data)): 
-        temp_data[i] = (int(temp_data[i],16))
-        
-    cred_send.append(' '.join(str(e) for e in temp_data))           # device ID
+	temp_data = cred_list[3].split(",")
+	for i in range(0, len(temp_data)): 
+		temp_data[i] = (int(temp_data[i],16))
+		
+	cred_send.append(' '.join(str(e) for e in temp_data))			# device ID
  
-    temp_data = cred_list[6].split(",")
-    for i in range(0, len(temp_data)): 
-        temp_data[i] = (int(temp_data[i],16))
-        
-    cred_send.append(' '.join(str(e) for e in temp_data))           # MASTER MAC
-    
-    temp_data = cred_list[7].split(",")
-    for i in range(0, len(temp_data)): 
-        temp_data[i] = (int(temp_data[i],16))
-        
-    cred_send.append(' '.join(str(e) for e in temp_data))           # USAR MAC
+	temp_data = cred_list[6].split(",")
+	for i in range(0, len(temp_data)): 
+		temp_data[i] = (int(temp_data[i],16))
+		
+	cred_send.append(' '.join(str(e) for e in temp_data))			# MASTER MAC
+	
+	temp_data = cred_list[7].split(",")
+	for i in range(0, len(temp_data)): 
+		temp_data[i] = (int(temp_data[i],16))
+		
+	cred_send.append(' '.join(str(e) for e in temp_data))			# USAR MAC
 
-    str1 = ' '.join(str(e) for e in cred_send)
-    print str1
-    
+	str1 = ' '.join(str(e) for e in cred_send)
+	print str1
+	
 def Write_Hex_to_bin_64(file_path,Device_Type_temp):
 	global temp
 	bl_frame_64_data = ""
@@ -292,9 +291,9 @@ def Write_Hex_to_bin_64(file_path,Device_Type_temp):
 	try:
 		f = open(file_path,'r')
 		for line in f:
-			length = int(line[1:3],16)/2 			# data length in bytes# 1st two chrecter are length  # 1:3 mease extrect 1 and 2nd charecter from line #int(line[1:2], 16)
-			address = int(line[3:7],16) | (ext_add<<16) 	# hex file address	# 3rd 4th 5th and 6th char are address 
-			dat_type =  int(line[7:9],16)
+			length = int(line[1:3],16)/2			# data length in bytes# 1st two chrecter are length	 # 1:3 mease extrect 1 and 2nd charecter from line #int(line[1:2], 16)
+			address = int(line[3:7],16) | (ext_add<<16)		# hex file address	# 3rd 4th 5th and 6th char are address 
+			dat_type =	int(line[7:9],16)
 			line_lnt=len(line)
 			if (line_lnt > 3):
 				data=line[9:(line_lnt-3)]  # data field
@@ -388,7 +387,7 @@ def reponce(cmd,resp):
 def print_heading():
 	print("\n -----------Boothost For PIC16F18313--------------\n")
 	print("By Nandkumar G Dhavalikar (nandhavalikar@gmail.com)\n")
-	print("K : Convert Hex to OTA\nF : Get New Fect Conf\nR : Read Flash\nP : Program \nC : Clear Screen\nI : Ping\nV : Read Boot loader Version\nT : Catch BL\nH : Help \nQ : Exit \n")
+	print("K : Convert Hex to OTA\nF : Get New Fect Conf\nU : Get New User Cred\nR : Read Flash\nP : Program \nC : Clear Screen\nI : Ping\nV : Read Boot loader Version\nT : Catch BL\nH : Help \nQ : Exit \n")
 def send_data(list_dat):
 	ser.reset_input_buffer()
 	ser.reset_output_buffer()
@@ -398,9 +397,9 @@ def send_data(list_dat):
 
 list_app_invalid = [EEP_WR_CMD, EEP_FDR_APP_VALID, 0xF0, NO_DEVICE] # [cmd add1 add0 data] write 00 at location 0x70FF of eeprom 
 list_app_valid =   [EEP_WR_CMD, EEP_FDR_APP_VALID, 0xF0, DEMO] # [cmd add1 add0 data] write 0xAA at location 0x70FF of eeprom 
-list_reset = 	   [RESET_CMD]
-list_ping = 	   [PING_CMD]
-list_Bl_ver = 	   [EEP_RD_CMD, EEP_FECT_BL_VER, 0xF0] # read bl version no
+list_reset =	   [RESET_CMD]
+list_ping =		   [PING_CMD]
+list_Bl_ver =	   [EEP_RD_CMD, EEP_FECT_BL_VER, 0xF0] # read bl version no
 
 
 try:
@@ -416,29 +415,29 @@ while (1) :
 	option = raw_input("\nOption:") #read user input
 	if ((option == 'K') or (option == 'k')):
 		Hex_File_Path = raw_input("\nEnter complete Hex File path : ") #read hex file path	
-        Device_Typ = raw_input("\nEnter Device  1:DEMO 2: Wall Switch ? : ") #read hex file path	
-        Device_Typ = int(Device_Typ)
-        if((Device_Typ > 0) and (Device_Typ < 3)):
-           print("\n Hex File Entered : " + Hex_File_Path)
-           Write_Hex_to_String_64(Hex_File_Path,Device_Typ)
-        else:
-            print "Data Input Error"
+		Device_Typ = raw_input("\nEnter Device	1:DEMO 2: Wall Switch ? : ") #read hex file path	
+		Device_Typ = int(Device_Typ)
+		if((Device_Typ > 0) and (Device_Typ < 3)):
+		   print("\n----Log---\nHex File Entered : " + Hex_File_Path)
+		   Write_Hex_to_String_64(Hex_File_Path,Device_Typ)
+		else:
+			print "Data Input Error"
 	elif ((option == 'F') or (option == 'f')):
-        Fect_File_Path = raw_input("\nEnter File path to store fectory OTA file: ") #read hex file path	
-        Device_Typ = raw_input("\nEnter Device  1:DEMO 2: Wall Switch ? : ") #read hex file path	
-        Device_Typ = int(Device_Typ)
-        if((Device_Typ > 0) and (Device_Typ < 3)):
-           Write_Hex_to_String_64(Fect_File_Path,Device_Typ)
-        else:
-            print "Data Input Error"
+		Fect_File_Path = raw_input("\nEnter File path to store factory OTA file: ") #read hex file path	
+		Device_Typ = raw_input("\nEnter Device	1:DEMO 2:Wall Switch ? : ") #read hex file path	
+		Device_Typ = int(Device_Typ)
+		if((Device_Typ > 0) and (Device_Typ < 3)):
+		   Write_Fect_OTA_file(Fect_File_Path,Device_Typ)
+		else:
+			print "Data Input Error"
 	elif ((option == 'U') or (option == 'u')):
-        cred_File_Path = raw_input("\nEnter File path to store user credential: ") #read hex file path	
-        Device_Typ = raw_input("\nEnter Device  1:DEMO 2: Wall Switch ? : ") #read hex file path	
-        Device_Typ = int(Device_Typ)
-        if((Device_Typ > 0) and (Device_Typ < 3)):
-           Write_Hex_to_String_64(cred_File_Path)
-        else:
-            print "Data Input Error"		
+		cred_File_Path = raw_input("\nEnter File path to store user credential: ") #read hex file path	
+		Device_Typ = raw_input("\nEnter Device	1:DEMO 2:Wall Switch ? : ") #read hex file path	
+		Device_Typ = int(Device_Typ)
+		if((Device_Typ > 0) and (Device_Typ < 3)):
+		   Create_Credential(cred_File_Path)
+		else:
+			print "Data Input Error"		
 	elif ((option == 'P') or (option == 'p')):
 		ser.open()
 		time.sleep(1)
